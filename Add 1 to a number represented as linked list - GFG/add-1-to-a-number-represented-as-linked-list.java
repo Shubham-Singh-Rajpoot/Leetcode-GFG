dd8df70cc514f397617fa9c1c68015de9eba2130
Shubham-Singh-Rajpoot <112822100+Shubham-Singh-Rajpoot@users.noreply.github.com>
2023-06-22 13:57:39 +0530

    Added solution - LeetHub

---
+++ Add 1 to a number represented as linked list - GFG/add-1-to-a-number-represented-as-linked-list.java
@@ -0,0 +1,97 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+class Node
+{
+    int data;
+    Node next;
+    
+    Node(int x)
+    {
+        data = x;
+        next = null;
+    }
+}
+class GfG
+{
+    public static void printList(Node node) 
+    { 
+        while (node != null)
+        { 
+            System.out.print(node.data);
+            node = node.next; 
+        }  
+        System.out.println();
+    } 
+    public static void main(String args[])throws IOException
+        {
+            Scanner sc = new Scanner(System.in);
+            int t = sc.nextInt();
+            while(t-->0)
+                {
+                    String s = sc.next();
+                    Node head = new Node( s.charAt(0) - '0' );
+                    Node tail = head;
+                    for(int i=1; i<s.length(); i++)
+                    {
+                        tail.next = new Node( s.charAt(i) - '0' );
+                        tail = tail.next;
+                    }
+                    Solution obj = new Solution();
+                    head = obj.addOne(head);
+                    printList(head); 
+                }
+        }
+}
+// } Driver Code Ends
+
+
+/*
+class Node{
+    int data;
+    Node next;
+    
+    Node(int x){
+        data = x;
+        next = null;
+    }
+} 
+*/
+
+class Solution
+{
+    public static Node addOne(Node head) 
+    { 
+        int tr=1,carry=0;
+        head=rev(head);
+        Node temp=head,prev=null;
+        while(temp!= null)
+        {
+            tr=tr+temp.data+carry;
+            temp.data=tr%10;
+            carry=tr/10;
+            prev=temp;
+            temp=temp.next;
+            tr=0;
+        }
+        if(carry!=0)
+        {
+            Node tmp=new  Node(carry);
+            tmp.next=null;
+            prev.next=tmp;
+        }
+        return rev(head);
+    }
+    public static Node rev(Node head)
+    {
+        Node prev=null,curr=head,next=null;
+        while(curr!=null)
+        {
+            next=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=next;
+        }
+        return prev;
+    }
+}
