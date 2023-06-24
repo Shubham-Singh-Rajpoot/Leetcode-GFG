fc03ba421a282aa1802def92724cc41ca18ab6f9
Shubham-Singh-Rajpoot <112822100+Shubham-Singh-Rajpoot@users.noreply.github.com>
2023-06-24 20:26:01 +0530

    Added solution - LeetHub

---
+++ Prefix match with other strings - GFG/prefix-match-with-other-strings.java
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t>0)
+        {
+            int n = sc.nextInt();
+            String[] arr = new String[n];
+            for(int i=0;i<n;i++)
+            {
+                arr[i] = sc.next();
+            }
+            
+            int k = Integer.parseInt(sc.next());
+            
+            String str = sc.next();
+            
+            Solution obj = new Solution();
+            int ans = obj.klengthpref(arr,n,k,str);
+            System.out.println(ans);
+            
+            t--;
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Solution
+{
+    public int klengthpref(String[] arr, int n, int k, String str)
+    {
+        int cnt=0;
+        int i;
+        for(i=0;i<arr.length;i++)
+            if(arr[i].length()>=k&&str.length()>=k)
+                if(arr[i].substring(0,k).equals(str.substring(0,k)))
+                    cnt++;
+        return cnt;
+    }
+}
