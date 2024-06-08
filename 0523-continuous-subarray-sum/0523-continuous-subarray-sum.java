911b7415eeccfda2c1fd5741e12d6aeff455a041
Shubham Singh Rajpoot <112822100+Shubham-Singh-Rajpoot@users.noreply.github.com>
2024-06-08 23:47:02 +0530

    Time: 24 ms (44.31%), Space: 57.6 MB (63.39%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.java
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.java
@@ -1,23 +1,32 @@
-class Solution
-{
-    public boolean checkSubarraySum(int[] nums, int k)
-    {
-        int i,j;
-        for(i=1;i<nums.length;i++)//checking a subarray of length 2 having elements 0(0 is multiple of k)
-            if(nums[i] == 0 && nums[i-1] == 0)
-                return true;
-        for(i=1;i<nums.length;i++)
-        {
-            nums[i] += nums[i-1];//Prefix sum
-            if(nums[i] % k ==0)//if at any time Prefix sum become divisible by k
-                return true;
-            j=i;
-            while(j> 1 && nums[i] > k) {
-                if((nums[i] - nums[j-2]) % k ==0)//if sum of any n elements becomes divisible by k
+class Solution {
+    public boolean checkSubarraySum(int[] nums, int k) {
+        int n = nums.length;
+        Map<Integer, Integer> remainderMap = new HashMap<>();
+        int cumulativeSum = 0;
+        
+        // Step 1: Initialize Remainder Map
+        remainderMap.put(0, -1);
+        
+        // Step 2: Iterate Through the List
+        for (int i = 0; i < n; i++) {
+            // Step 3: Calculate Cumulative Sum
+            cumulativeSum += nums[i];
+            
+            // Step 4: Calculate Remainder
+            int remainder = k == 0 ? cumulativeSum : cumulativeSum % k;
+            
+            // Step 5: Update Remainder Map
+            if (remainderMap.containsKey(remainder)) {
+                // Check if segment length is at least two
+                if (i - remainderMap.get(remainder) > 1) {
                     return true;
-                j--;//checking all previous numbers
+                }
+            } else {
+                remainderMap.put(remainder, i);
             }
         }
+        
+        // Step 6: Return the Result
         return false;
     }
 }
