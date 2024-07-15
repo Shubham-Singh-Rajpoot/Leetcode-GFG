5ce51ea565f50246b90cfab713bf7a18d90602bc
Shubham Singh Rajpoot <112822100+Shubham-Singh-Rajpoot@users.noreply.github.com>
2024-07-15 15:39:20 +0530

    Time: 62 ms (83.11%), Space: 54.8 MB (98.68%) - LeetHub

--- 2306-create-binary-tree-from-descriptions/2306-create-binary-tree-from-descriptions.java
+++ 2306-create-binary-tree-from-descriptions/2306-create-binary-tree-from-descriptions.java
@@ -16,23 +16,22 @@
 class Solution {
     public TreeNode createBinaryTree(int[][] descriptions) {
         HashMap<Integer,TreeNode> map=new  HashMap<>();
-        HashSet<Integer> set=new  HashSet<>();
-        for(int i=0;i<descriptions.length;i++)
-            set.add(descriptions[i][0]);
         for(int i=0;i<descriptions.length;i++)
         {
             if(!map.containsKey(descriptions[i][0]))
                 map.put(descriptions[i][0],new  TreeNode(descriptions[i][0]));
             if(!map.containsKey(descriptions[i][1]))
                 map.put(descriptions[i][1],new  TreeNode(descriptions[i][1]));
-            if(set.contains(descriptions[i][1]))
-                set.remove(descriptions[i][1]);
             if(descriptions[i][2]==1)
                 map.get(descriptions[i][0]).left=map.get(descriptions[i][1]);
             else
                 map.get(descriptions[i][0]).right=map.get(descriptions[i][1]);
         }
-        Iterator<Integer> it=set.iterator();
-        return map.get(it.next());
+        for(int i=0;i<descriptions.length;i++)
+            if(map.containsKey(descriptions[i][1]))
+                map.remove(descriptions[i][1]);
+        for(Map.Entry<Integer,TreeNode> set:map.entrySet())
+            return set.getValue();
+        return null;
     }
 }
