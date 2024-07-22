c85f912bcc9c069a6565a01a5cbb479e63a038dd
Shubham Singh Rajpoot <112822100+Shubham-Singh-Rajpoot@users.noreply.github.com>
2024-07-22 09:42:11 +0530

    Time: 10 ms (48.32%), Space: 45.5 MB (18.45%) - LeetHub

--- 2502-sort-the-people/2502-sort-the-people.java
+++ 2502-sort-the-people/2502-sort-the-people.java
@@ -1,12 +1,22 @@
 class Solution {
     public String[] sortPeople(String[] names, int[] heights)
     {
-        HashMap<Integer,String> map=new  HashMap<>();
-        for(int i=0;i<heights.length;i++)
-            map.put(heights[i],names[i]);
-        Arrays.sort(heights);
-        for(int i=0;i<heights.length;i++)
-            names[heights.length-i-1]=map.get(heights[i]);
+        int index=0;
+        String stemp;
+        int temp;
+        for(int i=0;i<heights.length-1;i++)
+        {
+            index=i;
+            for(int j=i+1;j<heights.length;j++)
+                if(heights[j]>heights[index])
+                    index=j;
+            temp=heights[index];
+            heights[index]=heights[i];
+            heights[i]=temp;
+            stemp=names[index];
+            names[index]=names[i];
+            names[i]=stemp;
+        }
         return names;
     }
 }
