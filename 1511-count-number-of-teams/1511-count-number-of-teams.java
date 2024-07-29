d7a92fbf11141ec149b8baaf736b6a990008307b
Shubham Singh Rajpoot <112822100+Shubham-Singh-Rajpoot@users.noreply.github.com>
2024-07-29 13:32:03 +0530

    Time: 23 ms (44.73%), Space: 42.6 MB (32.07%) - LeetHub

--- 1511-count-number-of-teams/1511-count-number-of-teams.java
+++ 1511-count-number-of-teams/1511-count-number-of-teams.java
@@ -9,36 +9,33 @@ class Solution
         Arrays.fill(large,-1);
         Arrays.fill(small,-1);
         int ans=0;
+        for(int j=1;j<rating.length-1;j++)
+            for(int k=j+1;k<rating.length;k++)
+            {
+                if(rating[k]>rating[j])
+                {
+                    if(large[j]==-1)
+                        large[j]=1;
+                    else
+                        large[j]++;
+                }
+                else
+                {
+                    if(small[j]==-1)
+                        small[j]=1;
+                    else
+                        small[j]++;
+                }
+            }
         for(int i=0;i<rating.length-2;i++)
         {
             for(int j=i+1;j<rating.length-1;j++)
             {
                 if(rating[j]>rating[i])
-                    ans+=(large[i]=traverse(rating,j,1));
+                    ans+=Math.max(large[j],0);
                 else
-                    ans+=(small[i]=traverse(rating,j,0));
+                    ans+=Math.max(small[j],0);
             }
-        }
-        return ans;
-    }
-    public int traverse(int[] rating,int i,int greater)
-    {
-        if(greater==0 && small[i]!=-1)
-            return small[i];
-        if(greater==1 && large[i]!=-1)
-            return large[i];
-        int ans=0;
-        if(greater==1)
-        {
-            for(int j=i+1;j<rating.length;j++)
-                if(rating[j]>rating[i])
-                    ans+=1;
-        }
-        else
-        {
-            for(int j=i+1;j<rating.length;j++)
-                if(rating[j]<rating[i])
-                    ans+=1;
         }
         return ans;
     }
