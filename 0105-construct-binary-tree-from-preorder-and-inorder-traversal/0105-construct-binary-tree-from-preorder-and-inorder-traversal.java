4b8d7873ad3744c44a95702957d8ea4744e70a60
Shubham Singh Rajpoot <112822100+Shubham-Singh-Rajpoot@users.noreply.github.com>
2024-08-01 16:08:54 +0530

    Time: 66 ms (5.34%), Space: 45.4 MB (11.92%) - LeetHub

---
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution
+{
+    HashMap<Integer,Integer> map=new  HashMap<>();
+    HashMap<Integer,Integer> premap=new  HashMap<>();
+    public TreeNode buildTree(int[] preorder, int[] inorder)
+    {
+        for(int i=0;i<inorder.length;i++)
+        {
+            map.put(inorder[i],i);
+            premap.put(preorder[i],i);
+        }
+        return traverse(preorder,inorder,0,inorder.length-1);
+        // TreeNode root=new TreeNode(preorder[0]);
+        // root.left=traverse(preorder,inorder,0,map.get(preorder[0])-1);
+        // root.right=traverse(preorder,inorder,map.get(preorder[0])+1,inorder.length-1);
+        // return root;
+    }
+    public TreeNode traverse(int preorder[],int inorder[],int i,int j)
+    {
+        if(i>j)
+            return null;
+        int minindex=inorder.length-1;
+        for(int k=i;k<=j;k++)
+            minindex=Math.min(minindex,premap.get(inorder[k]));
+        TreeNode temp=new  TreeNode(preorder[minindex]);
+        temp.left=traverse(preorder,inorder,i,map.get(preorder[minindex])-1);
+        temp.right=traverse(preorder,inorder,map.get(preorder[minindex])+1,j);
+        return temp;
+    }
+}
