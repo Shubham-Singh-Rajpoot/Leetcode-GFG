0a5e3b7095dfc0156c134e92043be96fceacc8c2
Shubham Singh Rajpoot <112822100+Shubham-Singh-Rajpoot@users.noreply.github.com>
2024-08-07 11:41:42 +0530

    Time: 0 ms (100%), Space: 41.5 MB (71.7%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -1,27 +1,23 @@
 class Solution {
-        public int numDecodings(String s)
-            {
-                    int n=s.length();
-                            int tbl[]=new  int[n+1];
-                                    Arrays.fill(tbl,Integer.MIN_VALUE);
-                                            return f1(0,s,tbl);
-                                                }
-                                                    public int f1(int i,String s,int[] tbl)
-                                                        {
-                                                                if(tbl[i]!=Integer.MIN_VALUE) return tbl[i];
-                                                                        if(i==s.length()) return tbl[i]=1;
-                                                                                if(i==s.length()-1)
-                                                                                            return tbl[i]=(s.charAt(i)>='1' && s.charAt(i)<='9')?1:0;
-                                                                                                    tbl[i]=0;
-                                                                                                            if(s.charAt(i) >='1' && s.charAt(i) <='9')
-                                                                                                                        tbl[i]+=f1(i+1,s,tbl);
-                                                                                                                                int val=10*(s.charAt(i)-'0')+(s.charAt(i+1)-'0');
-                                                                                                                                        if(val >=10 && val<=26)
-                                                                                                                                                    tbl[i]+=f1(i+2,s,tbl);
-                                                                                                                                                            return tbl[i];
-                                                                                                                                                                }
+    public int numDecodings(String s)
+    {
+        int n=s.length();
+        int tbl[]=new  int[n+1];
+        Arrays.fill(tbl,Integer.MIN_VALUE);
+        return f1(0,s,tbl);
+    }
+    public int f1(int i,String s,int[] tbl)
+    {
+        if(tbl[i]!=Integer.MIN_VALUE) return tbl[i];
+        if(i==s.length()) return tbl[i]=1;
+        if(i==s.length()-1)
+            return tbl[i]=(s.charAt(i)>='1' && s.charAt(i)<='9')?1:0;
+        tbl[i]=0;
+        if(s.charAt(i) >='1' && s.charAt(i) <='9')
+            tbl[i]+=f1(i+1,s,tbl);
+        int val=10*(s.charAt(i)-'0')+(s.charAt(i+1)-'0');
+        if(val >=10 && val<=26)
+            tbl[i]+=f1(i+2,s,tbl);
+        return tbl[i];
+    }
 }
- 
-
-   
-
