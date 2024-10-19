a639d705de235956fadd5ad5aeee1b9cb18a4244
Shubham Singh Rajpoot <112822100+Shubham-Singh-Rajpoot@users.noreply.github.com>
2024-10-19 22:04:03 +0530

    Time: 93 ms (39.3%), Space: 54.9 MB (14.78%) - LeetHub

--- 1667-find-kth-bit-in-nth-binary-string/1667-find-kth-bit-in-nth-binary-string.java
+++ 1667-find-kth-bit-in-nth-binary-string/1667-find-kth-bit-in-nth-binary-string.java
@@ -4,14 +4,15 @@ class Solution
     {
         if(n==1)
             return '0';
-        return traverse(n,k,1,"0");
+        return traverse(n,k,1,new StringBuilder("0"));
     }
-    public char traverse(int n,int k,int i,String s)
+    public char traverse(int n,int k,int i,StringBuilder s)
     {
         // System.out.println(n+" "+k+" "+i+" "+s);
         if(i==n)
             return s.charAt(k-1);
-        return traverse(n,k,i+1,s+"1"+reverseinvert(s));
+        s.append('1'+reverseinvert(s.toString()));
+        return traverse(n,k,i+1,s);
     }
     public String reverseinvert(String s)
     {
