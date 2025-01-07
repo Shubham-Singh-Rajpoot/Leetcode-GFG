37fc64cdf94e6562c90eb97466512d001c703af6
Shubham Singh Rajpoot <112822100+Shubham-Singh-Rajpoot@users.noreply.github.com>
2025-01-07 19:17:13 +0530

    Time: 1 ms (100%), Space: 42.1 MB (84.65%) - LeetHub

---
+++ 1524-string-matching-in-an-array/1524-string-matching-in-an-array.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<String> stringMatching(String[] words) {
+        List<String> res = new ArrayList<>();
+        int n = words.length;
+        for(int i = 0; i < n; i++)
+            if(checkSubstring(words, i, words[i], n))
+                res.add(words[i]);
+        return res;
+    }
+    boolean checkSubstring(String[] words, int idx, String subStr, int n)
+    {
+        for(int j = 0; j < n; j++)
+        {
+            if(idx==j)
+                continue;
+            if(words[j].contains(subStr))                
+                return true;
+        }
+        return false;
+    }    
+}
