d315ae3ac154238fefcd2838d80ab57f384b845e
Shubham Singh Rajpoot <112822100+Shubham-Singh-Rajpoot@users.noreply.github.com>
2025-02-10 23:19:26 +0530

    Time: 10 ms (5.96%), Space: 43 MB (14.62%) - LeetHub

---
+++ 3447-clear-digits/3447-clear-digits.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String clearDigits(String s)
+    {
+        List<Character> list=new  ArrayList<>();
+        for(int i=0;i<s.length();i++)
+        {
+            if(s.charAt(i)>=48 && s.charAt(i)<=57)
+            {
+                if(list.size()!=0 && (list.get(list.size()-1)<48 || list.get(list.size()-1)>57))
+                    list.remove(list.size()-1);
+                else
+                    continue;
+            }
+            else
+                list.add(s.charAt(i));
+        }
+        s="";
+        for(char ch:list)
+            s+=ch;
+        return s;
+    }
+}
