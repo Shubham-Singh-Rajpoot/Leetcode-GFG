eb79dc669b3b4561c8dc5d33a80a84d73af29a2f
Shubham Singh Rajpoot <112822100+Shubham-Singh-Rajpoot@users.noreply.github.com>
2025-09-11 13:39:03 +0530

    Time: 1267 ms (5.04%), Space: 46.2 MB (73.1%) - LeetHub

--- 2887-sort-vowels-in-a-string/2887-sort-vowels-in-a-string.java
+++ 2887-sort-vowels-in-a-string/2887-sort-vowels-in-a-string.java
@@ -1,27 +1,29 @@
 class Solution {
     public String sortVowels(String s) {
         ArrayList<Integer> indexes=new  ArrayList<>();
-        String vowels="aeiouAEIOU";
-        ArrayList<Character> vow=new  ArrayList<>();
+        String vowels="AEIOUaeiou";
+        int cnt[]=new int[10];
         ArrayList<Character> cons=new  ArrayList<>();
         for(int i=0;i<s.length();i++)
         {
             if(vowels.indexOf(s.charAt(i))!=-1)
             {
                 indexes.add(i);
-                vow.add(s.charAt(i));
+                cnt[vowels.indexOf(s.charAt(i))]++;
             }
             else
                 cons.add(s.charAt(i));
         }
-        Collections.sort(vow);
         StringBuilder str=new  StringBuilder();
+        int curr=0;
         while(indexes.size()!=0)
         {
+            while(cnt[curr]==0)
+                curr++;
             while(str.length()<indexes.get(0))
                 str.append(cons.remove(0));
-            str.append(vow.get(0));
-            vow.remove(0);
+            str.append(vowels.charAt(curr));
+            cnt[curr]--;
             indexes.remove(0);
         }
         while(cons.size()!=0)
