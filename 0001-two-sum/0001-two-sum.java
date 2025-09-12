492c8cedfb6da3b6198852b65331a2d5ad10fd0d
Shubham Singh Rajpoot <112822100+Shubham-Singh-Rajpoot@users.noreply.github.com>
2025-09-13 00:26:35 +0530

    Time: 102 ms (5.17%), Space: 45.1 MB (47.7%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -2,16 +2,17 @@ class Solution
 {
     public int[] twoSum(int[] nums, int target)
     {
-        int i,j;//ye do variables h index k liye
-        int ans[]=new int[2];//ye answer store krne k liye 2 size ki array h
-        for(i=0;i<nums.length;i++)//ye first index k liye loop
+        int i,j;
+        int ans[]=new int[2];
+        for(i=0;i<nums.length;i++)
         {
-            for(j=0;j<nums.length;j++)// ye 2nd index k liye loop h 
+            for(j=0;j<nums.length;j++)
             {
-                if(nums[i]+nums[j]==target && i!=j)//checking if the sum of values at index i and j equals to target
+                if(nums[i]+nums[j]==target && i!=j)
                 {
                     ans[0]=i;
                     ans[1]=j;
+                    break;
                 }
             }
         }
